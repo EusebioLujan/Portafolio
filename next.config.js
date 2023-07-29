45ea709f43fe4249ebf9170547a917f1a619ed6c
EusebioLujan <narboleta@gmail.com>
2023-07-29 05:23:52 -0300

    pre2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    target: 'serverless', // Habilita el enrutador de aplicaciones (App Router)
-    images: {
-      domains: ['lottie.host'],
-    },
-    // Otras configuraciones si las tienes
-  };
-  
-  module.exports = nextConfig;
+};
+
+module.exports = nextConfig;
+
