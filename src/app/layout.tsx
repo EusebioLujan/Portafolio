1bae3a05692e549aa0ca8a492c750450bdb03f20
EusebioLujan <narboleta@gmail.com>
2023-07-31 06:05:33 -0300

    predepl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-'use client'
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
