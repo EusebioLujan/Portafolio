5c32f3f6a21b169b6d82a6eaebce82d82c4a6dbf
EusebioLujan <narboleta@gmail.com>
2024-06-20 23:26:15 -0300

    release 24

--- src/app/components/about/page.tsx
+++ src/app/components/about/page.tsx
@@ -2,24 +2,27 @@
 import Image from "next/image";
 import React from "react";
 import about from "../../../../public/imageabout.jpg";
+
 export default function About() {
   return (
     <>
-    <p id="sobre-mi" className="my-20 md:my-1 text-[#0F212E] text-none">a</p>
-      <div  className=" w-10/12 min-h-screen px-10 flex flex-col justify-center items-center m-auto">
-        <h1 className="text-4xl md:text-6xl font-bold text-center underline  mb-20 md:mb-40">Sobre Mí</h1>
-        <div className=" flex items-center flex-col md:flex-row md:justify-around md:w-10/12">
-          <div className="w-10/12 relative z-40 flex justify-center">
+      <p id="sobre-mi" className="my-28 md:my-1 text-[#0F212E] text-none">a</p>
+      <div className="w-11/12 sm:w-10/12 min-h-screen px-4 sm:px-10 flex flex-col justify-center items-center m-auto">
+        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-center underline mb-10 sm:mb-20 md:mb-40">
+          Sobre Mí
+        </h1>
+        <div className="flex flex-col lg:flex-row items-center lg:justify-around lg:w-10/12 ">
+          <div className="w-full sm:w-10/12 md:w-8/12 lg:w-6/12 flex justify-center">
             <Image
-              className="rounded-full w-full md:w-7/12"
+              className="rounded-full w-8/12 sm:w-6/12 md:w-8/12 "
               src={about}
               alt="image"
               width={500}
               height={500}
             />
           </div>
-          <div className=" w-[250px] sm:w-full md:w-6/12 text-justify pt-8">
-            <h1 className="text-xl md:text-2xl">
+          <div className="w-full md:w-10/12 lg:w-6/12 text-justify pt-8 lg:pt-0">
+            <h1 className="text-lg sm:text-xl md:text-2xl">
               Tengo experiencia en HTML, CSS, lenguajes como JS y TS,
               frameworks como Next.js y Vue.js, y librerías como Tailwind CSS y
               Redux, entre otros. Me apasiona ver cómo, con un par de cambios en
--- src/app/components/social-media/page.tsx
+++ src/app/components/social-media/page.tsx
@@ -5,24 +5,26 @@ import { FaGithub, FaWhatsapp } from "react-icons/fa";
 export default function SocialMedia() {
   return (
     <>
-      <div className="fixed md:bottom-4 bottom-16 right-1 flex items-center w-2/12 justify-center mr-10 z-40">
-          <a
-            href="https://github.com/EusebioLujan"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="md:text-white text-textcolorv mr-4 hover:text-textcolorv hover:scale-110 z-40"
-          >
-            <FaGithub size={50} />
-          </a>
-          <a
-            href="https://api.whatsapp.com/send?phone=099249289"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="md:text-white text-textcolorv hover:text-textcolorv hover:scale-110 z-40"
-          >
-            <FaWhatsapp size={55} />
-          </a>
-        </div>
+      <div className="fixed md:bottom-4 bottom-8 -right-4 sm:-right-4 md:-right-20 lg:-right-40 xl:-right-60 flex items-center w-5/12 justify-center mr-10 z-40">
+        <div className="absolute inset-0 bg-gray-700/95 backdrop-blur-md rounded-full block md:hidden z-30 w-full"></div>
+
+        <a
+          href="https://github.com/EusebioLujan"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="md:text-white text-textcolorv mr-4 hover:text-textcolorv hover:scale-110 z-40"
+        >
+          <FaGithub size={50} />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send?phone=099249289"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="md:text-white text-textcolorv hover:text-textcolorv hover:scale-110 z-40"
+        >
+          <FaWhatsapp size={55} />
+        </a>
+      </div>
     </>
   );
 }
