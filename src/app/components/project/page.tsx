2aa134ac1fe1f2b0ff66163af9fb5dcb957cea78
marcelo <narboleta@gmail.com>
2024-07-07 11:05:42 -0300

    fix projects,add meme

--- src/app/components/project/page.tsx
+++ src/app/components/project/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import emercado from "../../../../public/emercado.png";
+import meme from "../../../../public/meme.png";
 import Link from "next/link";
 export default function Project() {
   return (
@@ -14,41 +14,40 @@ export default function Project() {
           Proyectos
         </h1>
         {/* proyecto 1 */}
-        {/* <div className="flex flex-col md:justify-around my-10 md:items-center">
+        <div className="flex flex-col md:justify-around my-10 md:items-center">
           <div className="flex flex-col md:w-6/12">
-            <h1 className="text-4xl">E-Mercado</h1>
+            <h1 className="text-4xl">lorem ipsum</h1>
             <div className="md:flex md:space-x-4">
-              <div className="bg-[#015560]/50 shadow-lg my-2 shadow-[#015560] md:w-10/12 h-64 rounded-r-2xl rounded-br-2xl flex items-end justify-end">
-                <div className="bg-slate-100 w-11/12 h-56 rounded-ss-2xl rounded-br-2xl flex justify-center items-center">
+              <div className="bg-[#015560]/50 shadow-lg my-2 shadow-[#015560] md:w-10/12 h-96 rounded-r-2xl rounded-br-2xl flex items-end justify-end">
+                <div className="bg-slate-100 w-11/12 h-full rounded-ss-2xl rounded-br-2xl flex justify-center items-center overflow-hidden">
                   <Link
-                    href={
-                      "https://eusebiolujan.github.io/workspaceinicial-JaP/"
-                    }
+                    href={"https://eusebiolujan.github.io/Portafolio/"}
                     target="_blank"
                   >
                     <Image
-                      className="w-full h-full relative z-20"
-                      src={emercado}
-                      alt="MERCADO"
+                      className="w-full h-full object-contain"
+                      src={meme}
+                      alt="meme"
                       width={500}
                       height={500}
                     />
                   </Link>
                 </div>
               </div>
+
               <div className="bg-slate-500/50 w-full rounded-xl my-2 md:p-4">
-                El proyecto de E-Mercado es una tienda en línea creada desde
-                cero utilizando únicamente HTML y CSS para diseñar y maquetar la
-                interfaz del sitio web. El objetivo principal es mostrar
-                productos de diferentes categorías y permitir a los usuarios
-                navegar por el catálogo de productos, visualizar detalles de
-                cada artículo y agregar los productos seleccionados al carrito
-                de compras. Es un proyecto en construcción, a medida que se vaya
-                mejorando agregare imagenes
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+                earum eligendi repellendus neque a, autem aspernatur error,
+                nesciunt nihil officia corrupti rem quos consectetur sapiente,
+                impedit quaerat modi rerum sunt? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Velit, assumenda voluptatum
+                possimus ab praesentium veniam, quae aliquid dignissimos
+                voluptas quisquam corporis dicta facere odit magni ipsum vel
+                aspernatur ipsam. Vero?
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
     </>
   );
